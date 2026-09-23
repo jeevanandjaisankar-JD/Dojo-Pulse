1c04d74f17e320b954c8382a9024b4678d9a64c9
Navya D <navyanandh16@gmail.com>
2026-09-23 13:54:04 +0530

    Update backend mentor profile

--- backend/src/config/mentors.js
+++ backend/src/config/mentors.js
@@ -9,63 +9,57 @@
 const MENTORS = [
   {
     id: "mentor-1",
-    username: "mentor1",
+    username: "aravind_r",
     password: "Aravind@123",
     name: "Aravind",
     email: "aravind.r@kalvium.com",
-    role: "Kalvium Dojo Mentor",
-    track: "Growth & Development",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=Aravind"
+    role: "Growth Mentor",
+    avatar: "https://media.licdn.com/dms/image/v2/D5603AQFVdky9mze8IA/profile-displayphoto-crop_800_800/B56ZmK_P2dJoAI-/0/1758973461407?e=1792022400&v=beta&t=_4Q4aX9nmfp8Ra9eB_v4Nb6upsQN2SQfHxyBk4QvMdg"
+  },
+  {
+    id: "campus-manager",
+    username: "ajay_balasubramanian",
+    password: "Ajay@123",
+    name: "Ajay Balasubramanian",
+    email: "ajay.balasubramaniam@kalvium.com",
+    role: "Kalvium Campus Manager",
+    avatar: "https://media.licdn.com/dms/image/v2/D5603AQFeeWBcOKY70g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1680778688134?e=1792022400&v=beta&t=HfpJDG6cmuAqX1wLWKXO8b6Bsi-Dv9AZWbasy8FlgEk"
   },
   {
     id: "mentor-2",
-    username: "mentor2",
-    password: "ChangeMe2!",
-    name: "Mentor 02",
-    email: "mentor2@dojo.local",
+    username: "dharini_s",
+    password: "Dharini@123",
+    name: "Dharini",
+    email: "dharani@kalvium.com",
     role: "Kalvium Dojo Mentor",
-    track: "Batch Operations",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=mentor-2"
+    avatar: "https://scontent-maa3-1.cdninstagram.com/v/t51.82787-19/658541095_17921862777289011_6986807217871659660_n.jpg?_nc_cat=108&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=rbulusiAKfoQ7kNvwG6kX_u&_nc_oc=AdoB1i0KhPFKLGEPI-I5Nfif1ocBBsn6pdfAbT_b4v04qKm-SaRJeP6w4yv43GCqYbk&_nc_zt=24&_nc_ht=scontent-maa3-1.cdninstagram.com&_nc_gid=oPc1O4RrFH0JYGWCApCCug&_nc_ss=7baaf&oh=00_AQI8-k7Sg4VIGASH2uXqH5oV8VveH6z-2E5N5-NqwOsVwA&oe=6AB92CE9"
   },
   {
     id: "mentor-3",
-    username: "mentor3",
-    password: "ChangeMe3!",
-    name: "Mentor 03",
-    email: "mentor3@dojo.local",
+    username: "mehtab_singh",
+    password: "Mehtab@123",
+    name: "Mehtab Singh",
+    email: "mehtab.s@kalvium.com",
     role: "Kalvium Dojo Mentor",
-    track: "Student Success",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=mentor-3"
-  },
-  {
-    id: "mentor-4",
-    username: "mentor4",
-    password: "ChangeMe4!",
-    name: "Mentor 04",
-    email: "mentor4@dojo.local",
-    role: "Kalvium Dojo Mentor",
-    track: "Programming Foundations",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=mentor-4"
+    avatar: "https://media.licdn.com/dms/image/v2/D5603AQGdQklMX80s8w/profile-displayphoto-crop_800_800/B56Z7BSbApIMAI-/0/1781359293165?e=1792022400&v=beta&t=HMD_ZZupc4xhupVhKeH3qY2Nfp3FY5utZ3NmNJwdS-k"
   },
   {
     id: "mentor-5",
-    username: "mentor5",
-    password: "ChangeMe5!",
-    name: "Mentor 05",
-    email: "mentor5@dojo.local",
+    username: "bibin_b",
+    password: "Bibin@123",
+    name: "Bibin B",
+    email: "bibin.b@kalvium.com",
     role: "Kalvium Dojo Mentor",
-    track: "Full-Stack Development",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=mentor-5"
+    avatar: "https://lh3.googleusercontent.com/a-/ALV-UjW7n9LGvkNMcLlmLZvrb_lWBMUUvdbyW3_IXTFpiPEQsKFO1jU=s480-p-k-rw-no"
   },
   {
     id: "mentor-6",
-    username: "mentor6",
-    password: "ChangeMe6!",
-    name: "Mentor 06",
-    email: "mentor6@dojo.local",
+    username: "sam_sperjan",
+    password: "Sam@123",
+    name: "Sam Sperjan",
+    email: "sam.s@kalvium.com",
     role: "Kalvium Dojo Mentor",
-    track: "Assessment Quality",
-    avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=mentor-6"
+    avatar: "https://lh3.googleusercontent.com/a-/ALV-UjX4lxSkUv6GHlSOMCTj4kSHuunV16G-EfXfTjEzppKU0vdQ1GI=s480-p-k-rw-no"
   },
   {
     id: "mentor-7",
